5bc78f78de249d332cd32f9ac13185046de5f37c
yanndepps <yann.deperrot@protonmail.com>
2023-06-10 13:20:51 +0200

    raylib boilerplate

--- smoothlife_gpu.c
+++ smoothlife_gpu.c
@@ -1,7 +1,21 @@
+#include <raylib.h>
 #include <stdio.h>
 
 int main(void)
 {
-  printf("Hello, Seaman!\n");
+  // smol RL inits
+  int screen_width = 800;
+  int screen_height = 600;
+  InitWindow(screen_width, screen_height, "SmoothLife");
+  SetTargetFPS(60);
+
+  // organize the event loop
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RED);
+    EndDrawing();
+  }
+
+  //---
   return 0;
 }
